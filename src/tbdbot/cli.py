c9506daef806ee26cd002421327347b0fbd2535a
Mr.Zergling <mr.zergling@gmail.com>
2019-11-21 20:55:48 -0800

    Finish moving old emoji counter into a proper cog

--- src/tbdbot/cli.py
+++ src/tbdbot/cli.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-from tbdbot.emoji_counter import backfill_emoji_count
+from tbdbot.cogs.emoji import EmojiCog
 from tbdbot.cogs.echo import Hello
 import argparse
 import json
@@ -11,18 +11,10 @@ bot = commands.Bot(BOT_PREFIX)
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
 
-async def on_message_bak(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('count emoji please') and await client.is_owner(message.author):
-        async with message.channel.typing():
-            await message.channel.send('counting emoji in messages and reactions for last 20000 messages in each channel')
-            await backfill_emoji_count(message)
-
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--apikey')
     args = parser.parse_args()
     bot.add_cog(Hello(bot))
+    bot.add_cog(EmojiCog(bot))
     bot.run(args.apikey)
