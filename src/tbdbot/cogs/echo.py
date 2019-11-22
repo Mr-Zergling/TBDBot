a8ef0059247af2e07eae76d41ec2f71af4a1894b
Mr.Zergling <mr.zergling@gmail.com>
2019-11-21 22:53:42 -0800

    add a few more basic ping/version commands, just getting a handle on setting up multiple commands in a cog

--- src/tbdbot/cogs/echo.py
+++ src/tbdbot/cogs/echo.py
@@ -1,8 +1,22 @@
 from discord.ext import commands
 
+def is_bot_bullshit(ctx):
+    return ctx.message.channel.id == 647172167284555779
+
 class Hello(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+
+    @commands.command(name="version")
+    @commands.check(is_bot_bullshit)
+    async def version(self, ctx):
+        await ctx.message.channel.send("TBD Bot V0.00001 PrePrePrePre...-Alpha")
+
+    @commands.command(name="ping")
+    @commands.check(is_bot_bullshit)
+    async def ping(self, ctx):
+        await ctx.message.channel.send(f"Pong {str(ctx.message.author)}, you're welcome")
 
 
     @commands.group(pass_context=True)
@@ -13,4 +27,6 @@ class Hello(commands.Cog):
 
     @hello.command()
     async def son(self, ctx):
-        await ctx.message.channel.send('HI DAD!1!1!!1ONEONE!1!1!')
+        await ctx.message.channel.send('HI DAD!1!1!!1ONE!1!ONE1!')
+
+
