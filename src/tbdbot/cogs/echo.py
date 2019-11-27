a5c210e4e58ed8774045cfe1215cef5536fb15d4
Mr.Zergling <mr.zergling@gmail.com>
2019-11-26 23:18:28 -0800

    add disk and percentages to sysinfo

--- src/tbdbot/cogs/echo.py
+++ src/tbdbot/cogs/echo.py
@@ -43,11 +43,14 @@ class InfoCog(commands.Cog):
     @commands.check(is_bot_channel)
     async def sysinfo(self, ctx):
         memory = psutil.virtual_memory()
+        disk = psutil.disk_usage("/")
         info = f"""```
 {psutil.cpu_count()} CPUs @ {psutil.cpu_freq().current} Mhz
 CPU %: {psutil.cpu_percent(interval=1)}
+Used Memory (MB): {int(memory.used /1024/1024)} ({memory.percent}%)
 Total Memory (MB): {int(memory.total/1024/1024)}
-Available Memory (MB): {int(memory.available /1024/1024)}
+Used Disk (/,MB): {int(disk.used/1024/1024)} ({disk.percent}%)
+Total Disk (/,MB): {int(disk.total/1024/1024)}
 Plaform: {platform.platform()}```"""
         await self.bot.reply(ctx, info)
 
