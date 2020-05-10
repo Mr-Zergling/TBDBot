d8bfa0c4daef6e8dd3fbdb241c882ec2031c3839
Mr.Zergling <mr.zergling@gmail.com>
2020-05-09 20:52:00 -0700

    Add detekt and ktlint and fix some mine formatting errors

--- src/main/kotlin/bot/tbd/TBDBot.kt
+++ src/main/kotlin/bot/tbd/TBDBot.kt
@@ -7,10 +7,10 @@ import com.gitlab.kordlib.core.event.message.MessageCreateEvent
 import com.gitlab.kordlib.core.on
 import kotlinx.coroutines.runBlocking
 import mu.KotlinLogging
-import java.time.Instant
 
 private val log = KotlinLogging.logger {}
 
+@Suppress("UtilityClassWithPublicConstructor")
 class TBDBotRunner {
     companion object {
         @JvmStatic
@@ -24,16 +24,19 @@ object TBDBot {
     suspend fun run() {
         log.info("Logging in....")
         val client = Kord(Config.global.apiToken)
+
         client.on<ReadyEvent> {
             val self = client.getSelf()
             log.info(
                 "Logged in as ${self.username}#${self.discriminator}"
             )
         }
+
         client.on<MessageCreateEvent> {
             val message = this.message
             log.info("${message.author?.username}#${message.author?.discriminator}: ${message.content}")
         }
+
         client.login()
     }
 }
--- src/main/kotlin/bot/tbd/config/ConfigStore.kt
+++ src/main/kotlin/bot/tbd/config/ConfigStore.kt
@@ -3,12 +3,12 @@ package bot.tbd.config
 import com.beust.klaxon.Klaxon
 import java.io.File
 
-object Config{
+object Config {
     private const val BOT_CONFIG_PATH = "bot_config.json"
 
     val global: BotConfig
 
-    init{
+    init {
         global = Klaxon().parse<BotConfig>(File(BOT_CONFIG_PATH))!!
     }
 }
