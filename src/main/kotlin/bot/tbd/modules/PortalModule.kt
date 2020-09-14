6599824ff9f3aac06e74f16a3eaa5764a0e1948d
Mr.Zergling <mr.zergling@gmail.com>
2020-09-13 21:21:26 -0700

    Make the portal module delete the command message so it's cleaner (now includes author avatar and name

--- src/main/kotlin/bot/tbd/modules/PortalModule.kt
+++ src/main/kotlin/bot/tbd/modules/PortalModule.kt
@@ -4,6 +4,7 @@ package bot.tbd.modules
 
 import bot.tbd.util.ResourceUrls
 import com.gitlab.kordlib.core.behavior.channel.createEmbed
+import com.gitlab.kordlib.core.behavior.edit
 import com.gitlab.kordlib.core.entity.Message
 import com.gitlab.kordlib.core.entity.channel.GuildMessageChannel
 import com.gitlab.kordlib.kordx.commands.annotation.AutoWired
@@ -19,24 +20,37 @@ val portalModule = module("portal") {
         invoke(ChannelArgument, StringArgument) { channel, msg ->
             when (channel) {
                 is GuildMessageChannel -> {
-                    val sourceUrl = createMessageLink(message)
-                    val destMessage = channel.createEmbed {
-                        title = "Portal from ${(message.getChannel() as GuildMessageChannel).name}"
-                        description = "[${message.getAuthorAsMember()?.displayName}: $msg]($sourceUrl)"
-                        color = Color.ORANGE
-                        thumbnail {
-                            url = ResourceUrls.orangePortal
-                        }
-                    }
-                    val destUrl = createMessageLink((destMessage))
-                    message.channel.createEmbed {
+                    val destMessage = channel.createMessage("Incoming portal....")
+                    val destUrl = createMessageLink(destMessage)
+                    val sourceMessage = message.channel.createEmbed {
                         title = "Portal to ${channel.name}"
-                        description = "[${message.getAuthorAsMember()?.displayName}: $msg]($destUrl)"
+                        description = "[$msg]($destUrl)"
                         color = Color.BLUE
                         thumbnail {
                             url = ResourceUrls.bluePortal
                         }
+                        author {
+                            name = "${message.getAuthorAsMember()?.displayName}"
+                            icon = message.author?.avatar?.url
+                        }
                     }
+                    val sourceUrl = createMessageLink(sourceMessage)
+                    destMessage.edit {
+                        content = ""
+                        embed {
+                            title = "Portal from ${(message.getChannel() as GuildMessageChannel).name}"
+                            description = "[$msg]($sourceUrl)"
+                            color = Color.ORANGE
+                            thumbnail {
+                                url = ResourceUrls.orangePortal
+                            }
+                            author {
+                                name = "${message.getAuthorAsMember()?.displayName}"
+                                icon = message.author?.avatar?.url
+                            }
+                        }
+                    }
+                    message.delete()
                 }
                 else -> respond("Invalid Channel Argument")
             }
